dad6b64f54108f257d5aa0bc59d7a9563f6c3a29
Sweetspotproject <deathlive.media@gmail.com>
2022-12-06 13:38:57 -0700

    Juego de adivinanza - Calcular numeros randomicos

---
+++ Logica de Programacion: Conceptos Primordiales/Juego de adivinacion/juego_Adivinar.js
@@ -0,0 +1,25 @@
+function saltarlinea(){
+    document.write("<br><br>");    
+}
+
+// Funcion de imprimir
+function imprimir(frase){
+    document.write(frase);
+    saltarlinea();
+}
+
+// Juego de adivinar
+var num_Pensado = Math.round(Math.random() * 10);
+var num_Ingrasado = parseInt(prompt("Ingrasa un numero entre 0-10"));
+
+if (num_Pensado == num_Ingrasado){
+
+    imprimir("Usted acerto !!!")
+
+}
+
+else {
+
+    imprimir("Usted no acerto, la respuesta era: " + num_Pensado + " :(");
+
+}
