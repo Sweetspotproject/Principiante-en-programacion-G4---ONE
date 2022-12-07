d4c52ecaea4550c3b77c21f02a3d4245b06fb771
Sweetspotproject <deathlive.media@gmail.com>
2022-12-07 12:40:56 -0700

    Formato a los archivos

--- Logica de Programacion: Conceptos Primordiales/Juego de adivinacion/juego_Adivinar.js
+++ Logica de Programacion: Conceptos Primordiales/Juego de adivinacion/juego_Adivinar.js
@@ -1,36 +1,32 @@
-function saltarlinea(){
-    document.write("<br><br>");    
+function saltarlinea() {
+  document.write("<br><br>");
 }
 
 // Funcion de imprimir
-function imprimir(frase){
-    document.write(frase);
-    saltarlinea();
+function imprimir(frase) {
+  document.write(frase);
+  saltarlinea();
 }
 
-
 // Juego de adivinar
-function sorteo(n){
-    return Math.round(Math.random()*n);
+function sorteo(n) {
+  return Math.round(Math.random() * n);
 }
 
 var num_Pensado = sorteo(10);
 var num_Ingrasado = parseInt(prompt("Ingresa un numero entre 0-10"));
 
-if (num_Pensado == num_Ingrasado){
-    imprimir("Usted Acerto !!!");
-}     
-else {
-    if(num_Ingrasado > num_Pensado){
-        imprimir("Usted no acerto, la respuesta menor a tu numero: " + num_Pensado + " :(");
-    }
-    else{
-        imprimir("Usted no acerto, la respuesta era mayor a tu numero")
-    }
+if (num_Pensado == num_Ingrasado) {
+  imprimir("Usted Acerto !!!");
+} else {
+  if (num_Ingrasado > num_Pensado) {
+    imprimir(
+      "Usted no acerto, la respuesta menor a tu numero: " + num_Pensado + " :("
+    );
+  } else {
+    imprimir("Usted no acerto, la respuesta era mayor a tu numero");
+  }
 }
-
-
-
 
 // Juego de adivinar
 /*
