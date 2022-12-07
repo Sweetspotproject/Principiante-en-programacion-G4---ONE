b37ca1ff8e0c653dd069fff4df8c46a3cff90565
Sweetspotproject <deathlive.media@gmail.com>
2022-12-07 11:52:47 -0700

    Aprendiendo condicionales if y else.

--- Logica de Programacion: Conceptos Primordiales/Juego de adivinacion/juego_Adivinar.js
+++ Logica de Programacion: Conceptos Primordiales/Juego de adivinacion/juego_Adivinar.js
@@ -8,7 +8,32 @@ function imprimir(frase){
     saltarlinea();
 }
 
+
 // Juego de adivinar
+function sorteo(n){
+    return Math.round(Math.random()*n);
+}
+
+var num_Pensado = sorteo(10);
+var num_Ingrasado = parseInt(prompt("Ingresa un numero entre 0-10"));
+
+if (num_Pensado == num_Ingrasado){
+    imprimir("Usted Acerto !!!");
+}     
+else {
+    if(num_Ingrasado > num_Pensado){
+        imprimir("Usted no acerto, la respuesta menor a tu numero: " + num_Pensado + " :(");
+    }
+    else{
+        imprimir("Usted no acerto, la respuesta era mayor a tu numero")
+    }
+}
+
+
+
+
+// Juego de adivinar
+/*
 var num_Pensado = Math.round(Math.random() * 10);
 var num_Ingrasado = parseInt(prompt("Ingrasa un numero entre 0-10"));
 
@@ -21,4 +46,4 @@ if (num_Pensado == num_Ingrasado){
     imprimir("Usted no acerto, la respuesta era: " + num_Pensado + " :(");
 
 }
-
+*/
