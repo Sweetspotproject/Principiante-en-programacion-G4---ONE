cd88963c1477a2c8dcedd553ac59a6c2ccb97933
sweetspot <deathlive.media@gmail.com>
2022-12-30 13:21:25 -0700

    Organizando codigo.

--- Logica de Programacion: Practicando con juegos y animaciones/Nuestra Pantalla tiene vida/pantallaViva.js
+++ Logica de Programacion: Practicando con juegos y animaciones/Nuestra Pantalla tiene vida/pantallaViva.js
@@ -1,28 +1,28 @@
 var pantalla = document.querySelector("canvas");
 var pincel = pantalla.getContext("2d");
-var colores = ['blue', 'red', 'green'];
-var contador = 0;
-
 pincel.fillStyle = "grey";
 pincel.fillRect(0,0,600,400);
-
-
-function cambiarColorClickDerecho(){
-    contador = (contador + 1) % colores.length;
-    var newColor = colores(contador);    
-}
+var colores = ['blue', 'red', 'green'];
+var colorActual = 0;
 
 
 function dibujarCirculoClickIzquierdo(evento){
     var x = evento.pageX - pantalla.offsetLeft;
     var y = evento.pageY - pantalla.offsetTop;
-    pincel.fillStyle = colores[contador];
+    pincel.fillStyle = colores[colorActual];
     pincel.beginPath();
     pincel.arc(x,y,10,0,2*3.14);
     pincel.fill();
     console.log(x + ", " + y);
     
 }
+
+
+function cambiarColorClickDerecho(){
+    colorActual = (colorActual + 1) % colores.length;
+    var newColor = colores(contador);    
+}
+
 pantalla.addEventListener('contextmenu', cambiarColorClickDerecho);
 pantalla.addEventListener('click', dibujarCirculoClickIzquierdo);
 
