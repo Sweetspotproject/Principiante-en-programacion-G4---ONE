bfddcde27ca58a7f12d7837810f27f80c887c536
sweetspot <deathlive.media@gmail.com>
2022-12-30 12:49:07 -0700

    Agregado funcion para cambiar color de pincel.

--- Logica de Programacion: Practicando con juegos y animaciones/Nuestra Pantalla tiene vida/pantallaViva.js
+++ Logica de Programacion: Practicando con juegos y animaciones/Nuestra Pantalla tiene vida/pantallaViva.js
@@ -1,12 +1,29 @@
 var pantalla = document.querySelector("canvas");
 var pincel = pantalla.getContext("2d");
+var colores = ['blue', 'red', 'green'];
+var contador = 0;
 
 pincel.fillStyle = "grey";
 pincel.fillRect(0,0,600,400);
 
-function alerta(evento){
-    console.log(evento);
-    alert("Usted hizo un clic");
+
+function cambiarColorClickDerecho(){
+    contador = (contador + 1) % colores.length;
+    var newColor = colores(contador);    
 }
 
-pantalla.onclick = alerta; 
+
+function dibujarCirculoClickIzquierdo(evento){
+    var x = evento.pageX - pantalla.offsetLeft;
+    var y = evento.pageY - pantalla.offsetTop;
+    pincel.fillStyle = colores[contador];
+    pincel.beginPath();
+    pincel.arc(x,y,10,0,2*3.14);
+    pincel.fill();
+    console.log(x + ", " + y);
+    
+}
+pantalla.addEventListener('contextmenu', cambiarColorClickDerecho);
+pantalla.addEventListener('click', dibujarCirculoClickIzquierdo);
+
+
